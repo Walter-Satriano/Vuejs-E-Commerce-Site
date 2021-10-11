b483cc7c6d961cf2b1b33a3892e907c1c720d9b8
Walter <walter@quomi.it>
2021-10-11 19:06:28 +0200

    - iterate over the products and for each product we display the information we have at our disposal
    - added a button to delete the product and his method implementation

--- src/views/ProductsList.vue
+++ src/views/ProductsList.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <h1>Product List</h1>
+  <div class="products-list">
+    <div v-for="(product, index) in $store.getters.products" :key="index">
+      <img :src="product.imageUrl">
+      <h2>{{ product.name }}</h2>
+      <p class="description">{{ product.description }}</p>
+      <p class="price">{{ product.price }} €</p>
+      <button @click="deleteProduct(index)">Delete product</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'ProductsList',
+  methods: {
+    deleteProduct(index) {
+      this.$store.commit('deleteProduct', index)
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .products-list {
+    display: flex;
+    padding-top: 30px;
+  }
+
+  img {
+    width: 300px;
+    height: auto;
+  }
+
+  .products-list div {
+    box-sizing: border-box;
+    padding: 30px;
+    background-color: lightsalmon;
+    margin: 0 20px;
+  }
+
+  button {
+    padding: 5px;
+    font-size: 1rem;
+  }
+
+  .description,
+  .price {
+    padding-top: 20px;
+  }
+</style>

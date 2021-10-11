4fc9f80a993a675625fcc212c7b1ffd06c59bcdd
Walter <walter@quomi.it>
2021-10-11 19:33:23 +0200

    showed the product details

--- src/views/SingleProduct.vue
+++ src/views/SingleProduct.vue
@@ -1,5 +1,50 @@
 <template>
-  <div>
-    <h1>Single Product</h1>
+  <div class="container">
+    <div class="content">
+      <h1>{{ product.name }}</h1>
+      <p class="description">{{product.description}}</p>
+      <p class="price">${{product.price}}</p>
+    </div>
+    <img class="image" :src="product.imageUrl" />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'SingleProduct',
+  data() {
+    return {
+      product: {}
+    }
+  },
+  mounted() {
+    this.product = this.$store.getters.products
+      .filter(item => item.slug === this.$route.params.slug)
+      .shift()
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    padding-top: 50px;
+    margin: 0 auto;
+    max-width: 1200px;
+  }
+
+  .image {
+    width: 500px;
+    height: auto;
+  }
+
+  .content h1 {
+    padding-bottom: 50px;
+  }
+
+  .description,
+  .price {
+    padding-top: 20px;
+  }
+</style>

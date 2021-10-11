cd1a4e2895bd47f816c0c31eeead1031c402bdf6
Walter <walter@quomi.it>
2021-10-11 18:02:59 +0200

    defined the markup of the form, mapped properties to the component data, added the addProduct event to the component and inside it, we interact with the Vuex store, which is accessible through this.$store

--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -1,5 +1,71 @@
 <template>
-  <div>
+  <form @submit.prevent="addProduct">
     <h1>Add Product</h1>
-  </div>
+    <div>
+      <label>Name:</label>
+      <input v-model="name" required>
+    </div>
+    <div>
+      <label>Price in €:</label>
+      <input v-model="price" required>  
+    </div>
+    <div>
+      <label>Description:</label>
+      <textarea v-model="description" required></textarea>
+    </div>
+    <div>
+      <label>Image URL:</label>
+      <input v-model="imageUrl" required>
+    </div>
+    <input type="submit" value="Add" class="button">
+  </form>
 </template>
+
+<script>
+export default {
+  name: "AddProduct",
+  data() {
+    return {
+      name: '',
+      price: 0,
+      description: '',
+      imageUrl: ''
+    }    
+  },
+  methods: {
+    addProduct() {
+      this.$store.commit('addProduct', {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        imageUrl: this.imageUrl
+      })
+    }
+  },
+}
+</script>
+
+<style scoped>
+  input,
+  textarea {
+    border: 1px solid #ccc;
+    padding: 20px;
+    vertical-align: middle;
+  }
+
+  label {
+    vertical-align: middle;
+    padding-right: 10px;
+  }
+
+  div {
+    display: block;
+    padding: 50px;font-size: 1.5rem;
+  }
+
+  .button {
+    background-color: #2c3e50;
+    color: white;
+    font-size: 1.5rem;
+  }
+</style>

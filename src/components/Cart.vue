6ebac6c2b901639074feeadcb274c76d1230533a
Walter <walter@quomi.it>
2021-10-12 18:13:55 +0200

    created the Cart component that appear only when one product is added into it

---
+++ src/components/Cart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="$store.getters.cart.length > 0">
+    <h2>CART</h2>
+
+    <div v-for="(item, index) in $store.getters.cart" :key="index">
+      <h3>{{item.product.name}}</h3>
+      <p>Quantity: {{item.quantity}}</p>
+      <p>Total Price: {{item.quantity * item.product.price}}</p>
+    </div>
+
+  </div>
+</template>

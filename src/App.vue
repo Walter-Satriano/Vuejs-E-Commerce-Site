c1bbf584ad9e117441e90fbb88894ca49d3a2944
Walter <walter@quomi.it>
2022-01-04 12:21:07 +0100

    deleted useless router link and imported Montserrat font from google font

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link to="/products">Products</router-link> |
       <router-link to="/add-product">Add Product</router-link> |
-      <a class="pointer" @click='toggleShowCart()'>Your Cart</a>
+      <a class="pointer" @click='toggleShowCart()'>Cart</a>
     </div>
 
     <Cart v-show="showCart" />
@@ -37,11 +35,11 @@ export default {
 
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Montserrat', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
   }
 

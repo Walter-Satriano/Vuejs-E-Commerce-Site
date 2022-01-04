b7e07e67e23879082f20bd3ab6e2c714aba28f97
Walter <walter@quomi.it>
2022-01-04 18:15:38 +0100

    styled button

--- src/views/ProductsList.vue
+++ src/views/ProductsList.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container-fluid bg-light py-5">
     <div class="row justify-content-center">
-      <div class="product-card position-relative col-sm-12 col-md-6 col-lg-4 col-xxl-3 text-center mb-5 px-0 mx-md-3"
+      <div class="product-card position-relative col-sm-12 col-md-6 col-lg-4 col-xxl-3 text-center mb-5 px-0 mx-md-3 bg-white"
         v-for="(product, index) in $store.getters.products" :key="index">
         <img :src="product.imageUrl" class="mb-4">
         <h2 class="text-uppercase">{{ product.name }}</h2>
         <p class="description">{{ product.description }}</p>
         <p class="price">{{ product.price }} €</p>
-        <button @click="$router.push(`/product/${product.slug}`)" class="mb-4">See Details</button>
-        
+        <button @click="$router.push(`/product/${product.slug}`)" class=" btn-lg mb-4">Product Details</button>
+
         <div class="position-absolute top-0 end-0 mt-2 me-2" @click.stop="deleteProduct(index)">
           <i class="bi bi-x-lg fs-3"></i>
         </div>
@@ -48,6 +48,17 @@
 
     .description {
       font-size: 1.2rem;
+    }
+
+    button {
+      background-color: rgb(179, 152, 1);
+      box-shadow: 3px 3px 6px #818181;
+      color: white;
+      border: none;
+
+      &:hover {
+        background-color: rgb(226, 193, 4);
+      }
     }
   }
 

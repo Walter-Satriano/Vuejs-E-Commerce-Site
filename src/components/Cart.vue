1a2f1130ea44a1fc70f9566d3ad7dc2acc2bcd9d
Walter <walter@quomi.it>
2022-01-07 18:30:46 +0100

    styled cart section

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="col-5">
-    <h2>CART</h2>
+  <div class="cart-container bg-light">
+    <div class="bg-black text-white py-2">
+      <h5>SUMMARY</h5>
+    </div>
     <div v-if="$store.getters.cart.length > 0">
-      <div v-for="(item, index) in $store.getters.cart" :key="index" class="d-flex">
+      <div v-for="(item, index) in $store.getters.cart" :key="index" class="d-flex px-3 py-3 align-items-center border-bottom border-3">
         <div>
           <img :src="item.product.imageUrl" alt="">
         </div>
-        <div>
+        <div class="ms-3">
           <h3 class="text-uppercase">{{item.product.name}}</h3>
-          <p>Quantity: {{item.quantity}}</p>
-          <p>Total Price: {{item.quantity * item.product.price}} €</p>
+          <p><span>Quantity: </span>{{item.quantity}}</p>
+          <p><span>Total Price: </span>{{item.quantity * item.product.price}} €</p>
           <button @click.stop="removeProductFromCart(index)">Remove</button>
         </div>
       </div>
     </div>
     <div v-else>
-      <p>YOUR CART IS EMPTY</p>
+      <p class="p-5 m-0"><b>HEY, YOUR CART IS EMPTY!</b></p>
     </div>
   </div>
 </template>
@@ -28,12 +30,39 @@
         this.$store.commit('removeProductFromCart', index)
       }
     }
-
   }
 </script>
 
 <style lang="scss" scoped>
-  img {
-    width: 150px;
+  .cart-container {
+    border: 1px solid black;
+    max-height: 355px;
+    overflow: auto;
+    box-shadow: 6px 6px 12px #818181;
+
+    img {
+      width: 150px;
+    }
+
+    p, h3 {
+      margin-bottom: 0.5rem;
+    }
+
+    span {
+      font-weight: 600;
+    }
+
+    button {
+      background-color: rgb(177, 3, 3);
+      box-shadow: 2px 2px 2px #818181;
+      color: white;
+      border: none;
+      border-radius: 5px;
+
+      &:hover {
+        background-color: rgb(240, 3, 3);
+      }
+    }
+
   }
 </style>
